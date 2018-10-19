a6e52fc09973e6fbffb8caae6cc341e17784cb4d
Maicon Vieira <maicon@Sirius.local>
2018-10-18 21:11:55 -0300

    First commit

--- parsing.rb
+++ parsing.rb
@@ -1,52 +1,102 @@
+#!/usr/bin/ruby
+
 require 'nokogiri'
 require 'pdf-reader'
 require 'open-uri'
-require 'uri'
+require 'fileutils'
 
+dir_base = "downloads"
 
-@doc = Nokogiri::HTML(open("http://legiscam.cvj.sc.gov.br/fusion/cvj/pautaComissao.jsp?id=138021"))
-re = /[0-9]*$/
-node = @doc.xpath("/html/body/div/table/tbody/tr[*]/td[1]/a/@href")
-node.each do |url|
-	url.to_s.scan(re) do |match|
-		teste = match.to_s
-		puts url + ": " + teste
-	end
+# http://legiscam.cvj.sc.gov.br/fusion/cvj/diarios.jsp
+# https://www.joinville.sc.gov.br/jornal
+
+commissions = ["http://legiscam.cvj.sc.gov.br/fusion/cvj/pautaComissao.jsp?id=138021"]#,
+#	"http://legiscam.cvj.sc.gov.br/fusion/cvj/pautaComissao.jsp?id=138018",
+#	"http://legiscam.cvj.sc.gov.br/fusion/cvj/pautaComissao.jsp?id=138034",
+#	"http://legiscam.cvj.sc.gov.br/fusion/cvj/pautaComissao.jsp?id=138037",
+#	"http://legiscam.cvj.sc.gov.br/fusion/cvj/pautaComissao.jsp?id=138031",
+#	"http://legiscam.cvj.sc.gov.br/fusion/cvj/pautaComissao.jsp?id=2315315",
+#	"http://legiscam.cvj.sc.gov.br/fusion/cvj/pautaComissao.jsp?id=138028",
+#	"http://legiscam.cvj.sc.gov.br/fusion/cvj/pautaComissao.jsp?id=2315448"]
+
+def txt_cleaning(text)
+  scraping = text.to_s
+#	scraping.gsub!(/^\s+/, '')
+#	scraping.gsub!(/^\*\n/, '')
+#	scraping.gsub!(/^CÂMARA DE VEREADORES DE JOINVILLE\nESTADO DE SANTA CATARINA\n/, '')
+#	scraping.gsub!(/^Divisão de Apoio às Comissões\n/,'')
+#	scraping.gsub!(/^Av\. Hermann August Lepper, 1\.100 - Saguaçu - CEP 89\.221-005 - Joinville\/SC\s+\d+\n/, '')
+#	scraping.gsub!(/^E-mail: camara@cvj\.sc\.gov\.br - Home page:www\.cvj\.sc\.gov\.br\n/, '')
+#	scraping.gsub!(/^Fone: \(47\) 2101-3333 - Fax: \(47\) 2101-3200$/, '')
+#	scraping.gsub!(/^COMISSÃO DE LEGISLAÇÃO, JUSTIÇA E REDAÇÃO\n/, '')
+#	scraping.gsub!(/^Coordenadoria Jurídica Legislativa\n/, '')
+#	scraping.gsub!(/^COMISSÃO DE URBANISMO, OBRAS, SERVIÇOS PÚBLICOS E MEIO AMBIENTE\n/, '')
+#	scraping.gsub!(/^Coordenadoria de Urbanismo, Obras, Serviços\n/, '')
+#	scraping.gsub!(/^COMISSÃO DE FINANÇAS, ORÇAMENTO E CONTAS DO MUNÍCIPIO\n/, '')
+#	scraping.gsub!(/^Coordenadoria de Finanças\n/, '')
+#	scraping.gsub!(/^COMISSÃO DE SAÚDE, ASSISTÊNCIA E PREVIDÊNCIA SOCIAL\n/, '')
+#	scraping.gsub!(/^Coordenadoria de Políticas Públicas\n/, '')
+#	scraping.gsub!(/^Comissão de Saúde, Assistência e Previdência Social\n/, '')
+#	scraping.gsub!(/^COMISSÃO DE EDUCAÇÃO, CULTURA, DESPORTO, CIÊNCIAS E TECNOLOGIA\n/, '')
+#	scraping.gsub!(/^Comissão de Educação, Cultura, Desporto, Ciências e Tecnologia\n/, '')
+#	scraping.gsub!(/^COMISSÃO DE CIDADANIA E DIREITOS HUMANOS\n/, '')
+#	scraping.gsub!(/^COMISSÃO DE PARTICIPAÇÃO POPULAR E CIDADANIA\n/, '')
+#	scraping.gsub!(/^Comissão de Participação Popular e Cidadania\n/, '')
+#	scraping.gsub!(/^COMISSÃO DE ECONOMIA, AGRICULTURA, INDÚSTRIA, COMÉRCIO E TURISMO\n/, '')
+#	scraping.gsub!(/^Comissão de Economia, Agricultura, Indústria, Comércio e Turismo\n/, '')
+#	scraping.gsub!(/^COMISSÃO DE PROTEÇÃO CIVIL E SEGURANÇA PÚBLICA\n/, '')
+#	scraping.gsub!(/^COMISSÃO DE PROTEÇÃO CIVIL E ANTIDROGAS\n/, '')
+#	scraping.gsub!(/^Comissão de Proteção Civil e Antidrogas\n/, '')
+	return scraping
 end
 
-#re = /[0-9]*$/m
-#str = 'http://legiscam.cvj.sc.gov.br:80/fusion/file/download/1511327'
+def id_extracting(text)
+	id = /[0-9]*$/.match(text).to_s
+	return id
+end
 
-# Print the match result
-#str.scan(re) do |match|
-#    puts match.to_s
-#end
+def year_extracting(text)
+	year = /(ORDINÁRIA|EXTRAORDINÁRIA|DE INSTALAÇÃO) - \d+\/\d+\/\d+/.match(text.to_s)
+	year = /[0-9]{4}$/.match(year.to_s).to_s
+	return year
+end
 
-#	puts url.URI.s
-#	p uri
-#	p uri.scheme
-#	p uri.host
+def commission_extracting(text)
+	commission = /(ORDINÁRIA|EXTRAORDINÁRIA|DE INSTALAÇÃO) - \d+\/\d+\/\d+/.match(text.to_s)
+	commission = /(ORDINÁRIA|EXTRAORDINÁRIA|DE INSTALAÇÃO)/.match(commission.to_s).to_s
+	return commission
+end
 
-#	puts uri
-#	io = open(uri)
-#	reader = PDF::Reader.new(io)
-#		reader.pages.each do |page|
-#		puts page.fonts
-#		puts page.text
-#		puts page.raw_content
-#	end
-# end
-
-#def add_keywords_to_profile(user)
-#  io = open(user.resume_pdf.to_s)
-#  reader = PDF::Reader.new(io)
-#  reader.pages.each do |page|
-#    string = page.text
-#    KeywordHelper.keywords.each do |word|
-#      if string.downcase.include?(word.downcase)
-#        user.keywords.push(word)
-#        user.save
-#      end
-#    end
-#  end
-#end
+commissions.each do |commission|
+	@doc = Nokogiri::HTML(open(commission))
+	node = @doc.xpath("/html/body/div/table/tbody/tr[*]/td[1]/a/@href")
+	node.each do |uri|
+#		id_extracted = id_extracting(uri)
+		filename = "#{id_extracting(uri)}.txt"
+#		folder = File.join("downloads", filename)
+		unless File.exist?(File.join(dir_base, filename))
+			io = open(uri)
+			reader = PDF::Reader.new(io)
+#			puts "Creating file: #{filename}"
+			reader.pages.each do |page|
+	#			year_extracted =
+				unless (year_extracting(page) == nil)
+					year_dir = File.join(dir_base, year_extracting(page))
+					FileUtils.mkdir_p year_dir unless File.exists?(year_dir)
+				end
+				unless (commission_extracting(page) == nil)
+					commission_dir = File.join(dir_base, year_extracting(page), commission_extracting(page))
+					FileUtils.mkdir_p commission_dir unless File.exists?(commission_dir)
+				end
+				scraped = txt_cleaning(page)
+				dir_and_file = File.join(commission_dir, filename)
+				unless File.exist?(dir_and_file)
+#					puts dir_and_file
+#					puts scraped
+					File.open(dir_and_file, "a+") { |f| f << "#{scraped}" }
+				end
+			end
+		 	puts "Finished!\n\n"
+		end
+	end
+end
